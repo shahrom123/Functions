13fd5b8538843f7b004dbedcf36d3ba935500157
Shahrom123 <shahrom909050409@gmail.com>
2024-05-08 21:10:54 +0500

    EvenCount

--- main.cpp
+++ main.cpp
@@ -1,5 +1,38 @@
 #include <iostream>
 using namespace std;
+
+int EvenCount(int n){
+
+  int n1,n2,n3,n4,n5,n6;
+  int sum = 0;
+  n1 = n%10;
+  n2 = n/10%10;
+  n3 = n/100%10;
+  n4 = n/1000%10;
+  n5 = n/10000%10;
+  n6 = n/100000;
+
+  if(n1%2==0)sum = sum + 1;
+  if(n2%2==0)sum = sum + 1;
+  if(n3%2==0)sum = sum + 1;
+  if(n4%2==0)sum = sum + 1;
+  if(n5%2==0)sum = sum + 1;
+  if(n6%2==0)sum = sum + 1;
+
+  return sum;
+
+}
+
+int main(){
+   int number;
+   cin>>number;
+   int sumEven = EvenCount(number);
+
+   cout<< sumEven;
+   main();
+}
+
+
 int MinDigit(int n){
      int n1,n2;
      n1 = n/10;
@@ -17,16 +50,3 @@ int MaxDigit(int n){
      if(n1>=n2) return n1;
      else return n2;
  }
-
-int main(){
-   int number;
-
-   cin>>number;
-
-   int Max = MaxDigit(number);
-   int Min= MinDigit(number);
-
-   cout<<"max: "<<Max<<endl;
-   cout<<"min: "<<Min;
-   main();
-}
